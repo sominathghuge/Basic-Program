531c31dc88401b9885882e518f87ec55a7e5d2ab
Sominath <101626540+sominathghuge@users.noreply.github.com>
2024-12-06 10:22:45 +0530

    Add files via upload
    first commit

---
+++ Assignment2_1.java
@@ -0,0 +1,17 @@
+package javaAssignments;
+
+public class Assignment2_1 {
+	
+	public static void main(String[] args) {
+		
+		for(int a=-100; a>=-150; a--)
+		{
+			System.out.println(a);
+		}
+		
+		
+		
+		
+	}
+
+}
